a298b98b60b0d35dfeadd0ecf7a6482cced4556f
klw2704 <32737025+klw2704@users.noreply.github.com>
2022-09-26 12:49:24 +0100

    Day 1

---
+++ Day 1/1_notes.py
@@ -0,0 +1,20 @@
+
+# Print function 
+print("Something")
+
+# Using quotations "" means its a string
+
+# To print on two lines using one command use \n
+print("Hello\nWorld!")
+
+# To concatenate a string 
+print("Hello " + "Kirsty")
+
+# The input function, input followed by prompt
+input("What is your name? ")
+
+print("Hello " + input("What is your name? "))
+
+# Variables
+name = input("What is your name? ")
+print(name)
---
+++ Day 1/Coding Rooms/input_length.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+
+name = input("What is your name? ")
+
+print(len(name))
+
+# OR
+
+print( len(input("What is your name? ")))
+
+# OR step-by-step
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
